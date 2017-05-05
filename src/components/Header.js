da6f330fe6b7c55c3b6be54d9fa362f937dc6b67
Malika Nallaperuma <indee89@gmail.com>
2017-05-05 22:03:42 +0530

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router';
+
+
+
+
+
+const Header =() =>(
+<div>
+<nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <a className="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+        <li className="active"><a href="#">Posts</a></li>
+      </ul>
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#">Sign in</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
+
+
+
+)
+
+export default Header;
